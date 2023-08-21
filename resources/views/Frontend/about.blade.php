2c9b18ed3789d561e9d0454ae22b9dac9770cd56
stdnt19 <mehakmehar502@gmail.com>
2023-08-21 11:22:26 +0500

    changing
    changing

--- resources/views/Frontend/about.blade.php
+++ resources/views/Frontend/about.blade.php
@@ -4,8 +4,9 @@
 
 
 
-        <div class="container">
+
             <img src="https://rstheme.com/products/html/shooter/shooter-html/images/banner/2.jpg" alt="">
+            <div class="container">
         <div class="row justify-content-center mt-3">
             <div class="col-lg-6 col-md-6 col-sm-6 ">
                 <h1>About <span style="color: #d32f2f;"> Raise Photographers</span></h1>
@@ -22,27 +23,27 @@
 
 
             <div class="row">
-                <div class="col-lg-9 col-md-12 col-sm-12">
+                <div class="col-lg-7 col-md-12 col-sm-12">
                         <h1>What <span style="color: #d32f2f;">We Offer</span></h1>
                         <div class="row mt-3">
-                            <div class="col-lg-9 col-md-12 col-sm-12">
+                            <div class="col-lg-7 col-md-12 col-sm-12">
                                 <h4 >Photographer Showcase:</h4>
                                 <p>Our platform is designed for photographers to showcase their portfolios. It's your canvas to highlight your unique skills, style, and the stories you tell through your lens.</p></div>
-                            <div class="col-lg-9 col-md-12 col-sm-12">
+                            <div class="col-lg-7 col-md-12 col-sm-12">
                                 <h4 >User-Friendly Search Filters:</h4>
                                 <p>Clients can effortlessly find and book photographers based on their location and budget. Our intuitive search filters simplify the process, ensuring clients discover the perfect photographer for their special moments.</p></div>
-                            <div class="col-lg-9 col-md-12 col-sm-12">
+                            <div class="col-lg-7 col-md-12 col-sm-12">
                                 <h4 >The Power of Feedback: </h4>
                                 <p>We understand the importance of feedback. Clients have the opportunity to leave reviews about their photographer's work, helping others make informed decisions and fostering a culture of continuous improvement.</p></div>
-                            <div class="col-lg-9 col-md-12 col-sm-12">
+                            <div class="col-lg-7 col-md-12 col-sm-12">
                                 <h4 >Photography Contests and Rankings:</h4>
                                 <p >We're passionate about recognizing outstanding photographers in our community. Our photography contests allow users to vote for their favorite photographers, resulting in a dynamic ranking system that showcases the best talents.</p></div>
-                            <div class="col-lg-9 col-md-12 col-sm-12">
+                            <div class="col-lg-7 col-md-12 col-sm-12">
                                 <h4 >Photography Equipment: </h4>
                                 <p>Whether you're a seasoned professional or just starting out, we've got you covered. Explore our extensive selection of photography equipment, including cameras, stands, lenses, and accessories. Elevate your creative journey with the right tools.</p></div>
                             </div>
                         </div>
-                        <div class="col-lg-3 g-4">
+                        <div class="col-lg-5 g-4">
                              <img src="https://rstheme.com/products/html/shooter/shooter-html/images/about-featured.png" alt=""></div>
                             </div>
 
