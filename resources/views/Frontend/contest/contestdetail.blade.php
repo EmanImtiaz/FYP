8c072fd2ba15082723074536a45f56401887a3cc
stdnt19 <mehakmehar502@gmail.com>
2023-09-06 13:23:15 +0500

    op
    op

--- resources/views/Frontend/contest/contestdetail.blade.php
+++ resources/views/Frontend/contest/contestdetail.blade.php
@@ -3,25 +3,27 @@
 
 <img src="https://rstheme.com/products/html/shooter/shooter-html/images/banner/2.jpg" alt="">
 
+
+
 <div class="container py-2">
     <div class="row">
         <div class="col-md-4">
             <div class="card">
-                <img src="https://images.pexels.com/photos/1643113/pexels-photo-1643113.jpeg?auto=compress&cs=tinysrgb&w=600" class="card-img-top" alt="Card Image">
+                <div class="position-relative">
+                    <img src="https://images.pexels.com/photos/1643113/pexels-photo-1643113.jpeg?auto=compress&cs=tinysrgb&w=600" class="card-img-top" alt="Card Image">
+                    <!-- Collection Icons (Font Awesome) on the top right -->
+                    <div class="position-absolute top-0 end-0 m-2">
+                        <i class="fas fa-heart fa-lg text-light"></i>
+                        <i class="fas fa-bookmark fa-lg text-light"></i>
+                    </div>
+                </div>
                 <div class="card-body">
-                    <div class="d-flex justify-content-between align-items-center">
-                        <!-- Collection Icons (Font Awesome) on the top right -->
-                        <div>
-                            <i class="fas fa-heart"></i>
-                            <i class="fas fa-bookmark"></i>
-                        </div>
-                        <!-- Photographer Profile Picture and Name on the left bottom -->
-                        <div class="d-flex align-items-center">
-                            <img src="https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=600" class="rounded-circle" alt="Photographer" height="100" width="100">
-                            <div class="ms-2">
-                                <h5 class="card-title">Photographer Name</h5>
-                                <p class="card-text">Image Description</p>
-                            </div>
+                    <!-- Photographer Profile Picture and Name on the bottom left -->
+                    <div class="d-flex align-items-center">
+                        <img src="https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=600" class="rounded-circle" alt="Photographer" height="100" width="100">
+                        <div class="ms-2">
+                            <h5 class="card-title">Photographer Name</h5>
+                            <p class="card-text">Image Description</p>
                         </div>
                     </div>
                 </div>
@@ -30,7 +32,6 @@
         <!-- You can add more col-md-4 divs for additional cards -->
     </div>
 </div>
-
 
 
 
