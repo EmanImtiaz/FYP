b5af6ec0e1cd8f60110f1096605409347b0612b4
EmanImtiaz <140343651+EmanImtiaz@users.noreply.github.com>
2023-09-12 23:02:10 +0500

    dvmdk

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\SigninController;
 use App\Http\Controllers\ResetController;
 use App\Http\Controllers\SignupController;
 use App\Http\Controllers\WinnersController;
+use App\Http\Controllers\HomeController;
 
 use App\Http\Controllers\BookinginfoController;
 use App\Http\Controllers\BookingformController;
