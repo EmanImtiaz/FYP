9dddb1f38abdf58c33dc9462b65a3c5debec0d87
EmanImtiaz <140343651+EmanImtiaz@users.noreply.github.com>
2023-12-18 12:22:14 +0500

    yjhtygh

---
+++ database/migrations/2023_12_15_153409_create_blog_posts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('blog_posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('url');
+            $table->longText('description');
+            $table->text('img');
+            $table->text('cover_img');
+            $table->boolean('is_active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('blog_posts');
+    }
+};
