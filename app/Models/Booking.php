06cca4f4d93256db9287e20742207afe7bb32785
stdnt19 <mehakmehar502@gmail.com>
2024-02-20 00:21:35 +0500

    c

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -9,7 +9,7 @@ class Booking extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['id','user_id','photographer_profile_id','name', 'email', 'phone', 'province_id','city_id','town_id','payment_method_options','evidence','account_name', 'account_no','is_paid', 'remarks','total_amount','payment_id'];
+    protected $fillable = ['id','user_id','photographer_profile_id','name', 'email', 'phone', 'province_id','city_id','town_id','payment_method_options','evidence','account_name', 'account_number','is_paid', 'remarks','total_amount','payment_id'];
 
     public function user()
     {
